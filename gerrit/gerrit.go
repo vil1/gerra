f8495f0de67b6fc35fbfef916308f95bb3a2fa3c
vil1 <valentin.kasas@gmail.com>
2013-07-02 23:41:54 +0200

    Documentation for gerrit package
    Documented exported types and functions

--- gerrit/gerrit.go
+++ gerrit/gerrit.go
@@ -20,11 +20,12 @@ func init() {
 
 }
 
+// Commit is the internal representation of gerrit's commit informations
 type Commit struct {
-	Project string
-	Branch  string
-	Id      string
-	Subject string
+	Project string // the project name
+	Branch  string // the branch this commit is included in
+	Id      string // the commit's SHA-1 id
+	Subject string // the commit message
 }
 
 func (c *Commit) getIssueKey() string {
@@ -35,12 +36,19 @@ type Client struct {
 	host, port string
 }
 
+// NewClient create a new instance of Client for the given host and port 
+// and returns a pointer to it.
 func NewClient(host, port string) *Client {
 	return &Client{host, port}
 }
 
-func (c *Client) GetIssueFromCommit(project, commit string) (key string) {
-	cmd := exec.Command("ssh", "-p", c.port, c.host, "gerrit", "query", "--format=JSON", "commit:"+commit, "project:"+project, "limit:1")
+// GetIssueFromCommit obtains the informations about the commit identified by sha1 in the given project.
+// It then extracts the issue key from the commit's message.
+// The extracted key is the first substring matching [A-Z]+-[0-9]+ found
+// in the commit message. If no key is found, a nil string is returned.
+// TODO : properly handle gerrit errors.
+func (c *Client) GetIssueFromCommit(project, sha1 string) (key string) {
+	cmd := exec.Command("ssh", "-p", c.port, c.host, "gerrit", "query", "--format=JSON", "commit:"+sha1, "project:"+project, "limit:1")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatalln(err)
